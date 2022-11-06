29c8aa04e91e05368926a810de72a14c7619d68f
Gabriel Nakic <gnakic@proton.me>
2022-11-06 19:41:49 +0100

    some solved exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,19 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+
+const BASE_APPLE_PRICE: i32 = 2;
+const DISCOUNT_APPLE_PRICE: i32 = 1;
+
+fn calculate_price_of_apples(n_of_apples: i32) -> i32 {
+    if (n_of_apples > 40) {
+        DISCOUNT_APPLE_PRICE * n_of_apples
+    } else {
+        BASE_APPLE_PRICE * n_of_apples
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -16,9 +16,7 @@
 // - The input is going to be a Vector of a 2-length tuple,
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
-// No hints this time!
-
-// I AM NOT DONE
+// Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
 pub enum Command {
     Uppercase,
@@ -30,20 +28,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let command_output = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().into(),
+                Command::Append(n) => string.to_owned() + &"bar".repeat(*n),
+            };
+
+            output.push(command_output);
         }
+
         output
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
